a7df4bbd47b8e7c3795e3e5255805611e142c70d
Tommy Donavon <donavontommy@gmail.com>
2025-07-16 13:56:47 -0600

    feat: enable biome language server

--- nvim/lua/lunavim/lsp.lua
+++ nvim/lua/lunavim/lsp.lua
@@ -2,12 +2,17 @@ vim.lsp.config('*', {
   root_markers = { '.git' },
 })
 
+vim.lsp.config('biome', {
+  root_markers = { 'biome.json' },
+})
+
 vim.lsp.enable {
   'astro',
   'elixirls',
   'golangci_lint_ls',
   'gopls',
   'graphql',
+  'biome',
   'hls',
   'jsonnet_ls',
   'lua_ls',
